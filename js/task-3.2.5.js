923f85bb669f1279a9aef4c9d11f777b6bb1fecc
JasminBurn <72577723+JasminBurn@users.noreply.github.com>
2020-11-10 21:09:41 +0000

    Task 3.2.5

---
+++ js/task-3.2.5.js
@@ -0,0 +1,11 @@
+var temp = 40;
+
+if (temp <= 50) {
+    console.log("You need to put on a coat");
+} else if (temp <=30) {
+    console.log("You need to put on a coat and hat");
+} else if (temp <=0) {
+    console.log("You need to stay inside");
+} else {
+    console.log("Just pants and a vest is fine!");
+}
