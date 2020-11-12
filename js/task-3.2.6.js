c28dd55a0346a34dc2ae0acf8f1d782ca90d9a8a
JasminBurn <72577723+JasminBurn@users.noreply.github.com>
2020-11-12 19:27:49 +0000

    Task 3.2.6

---
+++ js/task-3.2.6.js
@@ -0,0 +1,14 @@
+var temp = 51;
+var minutes = 34
+
+if (temp <= 50 && minutes >30) {
+    console.log("You need to put on a coat");
+} else if (temp <=30) {
+    console.log("You need to put on a coat and hat");
+} else if (temp <=0) {
+    console.log("You need to stay inside");
+} else {
+    console.log("Just pants and a vest is fine!");
+}
+
+// Logical operators && (AND) || (OR) ! (NOT)
