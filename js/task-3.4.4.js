b6b0393a7b22ba75c73a2a25b56ea49fab5c159d
JasminBurn <72577723+JasminBurn@users.noreply.github.com>
2020-11-19 18:52:03 +0000

    Update task-3.4.4.js

---
+++ js/task-3.4.4.js
@@ -0,0 +1,11 @@
+var lasagne = {
+    ingredients: ['Cheese', ' Pasta sheets', ' Tomato paste', ' Onions',' Salt', ' Pepper', ' Quorn mince'],
+    servings: 2,
+    directions: ["Fry", "grate"]
+}
+    
+function outputRecipe(recipe) {  
+        console.log('You want to make ' + recipe.servings + ' servings of lasagne, using the ingredients of: ' + recipe.ingredients + '.' + " You'll need to follow these steps: " + recipe.directions);
+    }
+
+outputRecipe(lasagne);
